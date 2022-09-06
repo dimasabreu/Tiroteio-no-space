5bb2e676c966261194b3440560ad802f90df4953
Dimas Abreu <dimasabreu@live.com>
2022-09-06 14:03:01 -0300

    movi a pasta de spites

---
+++ Space shooter/Assets/Scripts/Inimigo01Controller.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inimigo01Controller : MonoBehaviour
+{   // pegando o rb
+    private Rigidbody2D meuRB;
+    [SerializeField] private float velocidade = -3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // pegnado o rb
+        meuRB = GetComponent<Rigidbody2D>();
+        // colocando a velocidade no rb
+        meuRB.velocity = new Vector2(0f, velocidade);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
