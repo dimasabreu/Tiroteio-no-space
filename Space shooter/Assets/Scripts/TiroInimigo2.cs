c846114662e6be16ddd702a3f4511a43e4063bd2
Dimas Abreu <dimasabreu@live.com>
2022-09-13 20:43:30 -0300

    arrumei as pastas

---
+++ Space shooter/Assets/Scripts/TiroInimigo2.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TiroInimigo2 : MonoBehaviour
+{
+    private Rigidbody2D meuRB;
+    [SerializeField] private float vel = 10f;
+    // pegando a animação do final do tiro
+    [SerializeField] private GameObject impacto;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // rigidbody
+        meuRB = GetComponent<Rigidbody2D>();
+        // indo para cima
+        meuRB.velocity = new Vector2(0f, vel);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Inimigo"))
+        {
+            // pegando o metodo perde vida e aplicando o dano
+            collision.GetComponent<InimigoPai>().PerdeVida(1);
+        }
+        // checando se eu colidi com o player
+        if (collision.CompareTag("Jogador"))
+        {
+            collision.GetComponent<PlayerController>().PerdeVida(1);
+        }
+        Destroy(gameObject);
+
+        // criando o impacto do tiro
+        Instantiate(impacto, transform.position, transform.rotation);
+        
+    }
+}
