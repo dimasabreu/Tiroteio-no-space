c59dc5add544c4ace800e50f34ea2e73a39da90c
Dimas Abreu <dimasabreu@live.com>
2022-09-16 14:50:13 -0300

    colocando o tiro dentro da func e unico pra cada

--- Space shooter/Assets/Scripts/Inimigo02controller.cs
+++ Space shooter/Assets/Scripts/Inimigo02controller.cs
@@ -8,11 +8,10 @@ public class Inimigo02controller : InimigoPai
     
     // pegando a posição nova do tiro
     [SerializeField] private Transform posicaoTiro;
-    // meu tiro
-    [SerializeField] private GameObject meuTiro;
+   
     
 
-    private float esperaTiro = 1f;
+ 
     // Start is called before the first frame update
     void Start()
     {
@@ -27,6 +26,11 @@ public class Inimigo02controller : InimigoPai
     // Update is called once per frame
     void Update()
     {
+        Atirando();
+    }
+
+    private void Atirando()
+    {
         // Checar se a sprite esta visivel pegando info do child
         bool visivel = GetComponentInChildren<SpriteRenderer>().isVisible;
         // diminuir a espera
@@ -36,7 +40,20 @@ public class Inimigo02controller : InimigoPai
             if (esperaTiro <= 0)
             {
                 // Instanciando o tiro
-                Instantiate(meuTiro, posicaoTiro.position, transform.rotation);
+                var tiro = Instantiate(meuTiro, posicaoTiro.position, transform.rotation);
+                // encontrando o player
+                var player = FindObjectOfType<PlayerController>();
+                // encontrando o valor da direção
+                Vector2 direcao = player.transform.position - tiro.transform.position;
+                // normalizando a velocidade do tiro 
+                direcao.Normalize();
+                // dando a direcao e velo do tiro
+                tiro.GetComponent<Rigidbody2D>().velocity = direcao * velocidadeTiro;
+                // colocando o angulo do tiro
+                float angulo = Mathf.Atan2(direcao.y, direcao.x) * Mathf.Rad2Deg;
+                //passando o angulo para o tiro
+                tiro.transform.rotation = Quaternion.Euler(0f, 0f, angulo + 90f);
+                
                 // reiniciando o tiro
                 esperaTiro = Random.Range(2f, 4f);
             }
