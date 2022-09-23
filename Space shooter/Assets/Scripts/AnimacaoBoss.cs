ec4ab98c2fa20b61f1a1361e961079acec834362
Dimas Abreu <dimasabreu@live.com>
2022-09-22 22:53:53 -0300

    Create AnimacaoBoss.cs

---
+++ Space shooter/Assets/Scripts/AnimacaoBoss.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimacaoBoss : MonoBehaviour
+{
+    [SerializeField] private GameObject boss;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CriaBoss()
+    {
+        Instantiate(boss, transform.position, transform.rotation);
+    }
+}
