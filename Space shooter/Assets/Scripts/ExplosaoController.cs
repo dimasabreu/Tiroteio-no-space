a12553914befbf4ac01babda3678c5a396f10791
Dimas Abreu <dimasabreu@live.com>
2022-09-25 14:26:14 -0300

    Update ExplosaoController.cs

--- Space shooter/Assets/Scripts/ExplosaoController.cs
+++ Space shooter/Assets/Scripts/ExplosaoController.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 
 public class ExplosaoController : MonoBehaviour
 {
+    [SerializeField] private AudioClip meuSom;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(transform.position.y > -5.5f && transform.position.y < 5.5f)
+        {
+            AudioSource.PlayClipAtPoint(meuSom, Vector3.zero);
+        }
     }
 
     // Update is called once per frame
